b5d6d1982648a7042107f482829276ac5bbc4fe9
Lyman <lym_60740@hotmail.fr>
2024-02-26 12:10:45 +0100

    letter spacing

--- src/app/components/Model.jsx
+++ src/app/components/Model.jsx
@@ -19,10 +19,11 @@ const { nodes: nodesBarbed } = useGLTF("/medias/wire1.glb");
         position: { value: { x: 0, y: 0, z: -0.7 }, step: 0.1 },
         rotation: { value: { x: -1.7, y: -3.2, z: 0 }, step: 0.1 },
       });
-      const { positionFont,textFontSize, rotationFont } = useControls('Font Params', {
+      const { positionFont,textFontSize, rotationFont, letterSpace } = useControls('Font Params', {
         positionFont: { value: { x: 0, y: 0., z: -1.2 }, step: 0.1 },
         rotationFont: { value: { x: 0, y: 0, z: 0 }, step: 0.1 },
         textFontSize: { value: 0.8, min: 0.1, max: 5, step: 0.1 },
+        letterSpace: { value: 0.1, min: 0.001, max: 2, step: 0.1 },
       });
       const {  rotationSpeed } = useControls({
         
@@ -91,7 +92,7 @@ const { nodes: nodesBarbed } = useGLTF("/medias/wire1.glb");
     
     return (
         <group scale={viewport.width /3.5}> 
-        <Text ref={font} fontSize={textFontSize}  font="fonts\BrokenScript.otf" >
+        <Text ref={font} fontSize={textFontSize} letterSpacing={letterSpace}  font="fonts\BrokenScript.otf" >
     sohier
     
   </Text>
