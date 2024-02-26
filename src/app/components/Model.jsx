90c71a624b4d25d54565f0c9c6bc17b4e9cc85c3
Lyman <lym_60740@hotmail.fr>
2024-02-26 10:59:18 +0100

    ajout des controle size font et rotation speed

--- src/app/components/Model.jsx
+++ src/app/components/Model.jsx
@@ -9,39 +9,57 @@ export default function Model() {
     const mesh = useRef();
     const mesh1 = useRef();
     const groupRef = useRef();
-    const { nodes } = useGLTF("/medias/roseS.glb");
+    const barbed = useRef();
+    const { nodes: nodesRose } = useGLTF("/medias/roseS.glb");
+const { nodes: nodesBarbed } = useGLTF("/medias/wire1.glb");
+
     const { viewport } = useThree();
     const { position, rotation } = useControls('Transform', {
         position: { value: { x: 0, y: 0, z: -0.7 }, step: 0.1 },
         rotation: { value: { x: -1.7, y: -3.2, z: 0 }, step: 0.1 },
       });
-      const { position1, rotation1 } = useControls('Transform', {
-        position1: { value: { x: 0, y: 0.1, z: 0.5 }, step: 0.1 },
-        rotation1: { value: { x: -1.8, y: -1.4, z: -0.1 }, step: 0.1 },
+      const { position1, rotation1, scaleX, scaleY } = useControls('Transform', {
+        position1: { value: { x: 0, y: 0.1, z: 0 }, step: 0.1 },
+        rotation1: { value: { x: 0, y: 0, z: 0 }, step: 0.1 },
+      });
+      const { textFontSize, rotationSpeed } = useControls({
+        textFontSize: { value: 0.8, min: 0.1, max: 5, step: 0.1 },
+        rotationSpeed: { value: 0.01, min: 0.001, max: 0.1, step: 0.001 },
       });
     useFrame(({ mouse }) => {
-        // mesh.current.rotation.x = rotation.x;
-        // mesh.current.rotation.z += 0.01; 
-        // mesh.current.rotation.y = rotation.y;
+        
  
-        mesh1.current.rotation.x = rotation1.x;
-        mesh1.current.rotation.y = rotation1.y;
-        mesh1.current.rotation.z = rotation1.z;
+        mesh1.current.rotation.x = -1.8;
+        mesh1.current.rotation.y = -1.4;
+        mesh1.current.rotation.z = -0.1;
 
-        mesh1.current.position.x =position1.x;
-        mesh1.current.position.y = position1.y;
-        mesh1.current.position.z = position1.z; 
+        mesh1.current.position.x =0;
+        mesh1.current.position.y = 0.1;
+        mesh1.current.position.z = 0.5; 
+
+        // barbed.current.rotation.x = rotation1.x;
+        // barbed.current.rotation.y = rotation1.y;
+        // barbed.current.rotation.z = rotation1.z;
+        
+        // barbed.current.position.y = position1.y;
+        // barbed.current.position.x = position1.x;
+        // barbed.current.position.z = position1.z;
+
+
+
+
+        
 
 
         groupRef.current.rotation.x = rotation.x;
         groupRef.current.rotation.y = rotation.y;
-        groupRef.current.rotation.z += 0.01;
+        // groupRef.current.rotation.z += 0.005;
         
         groupRef.current.position.y = position.y;
         groupRef.current.position.x = position.x;
         groupRef.current.position.z = position.z;
 
-        // groupRef.current.rotation.z += 0.01;
+        groupRef.current.rotation.z += rotationSpeed;
 
       });
  
@@ -54,22 +72,29 @@ export default function Model() {
         chromaticAberration: {value: 0.02, min: 0, max: 1},
         backside: {value: true},
 
+
     })
 
     
     return (
         <group scale={viewport.width /3.5}> 
-        <Text fontSize={0.8} position={[0, 0, -1]} font="fonts\Oldenorth.otf">
-            SOHIER
-        </Text>
+        <Text fontSize={textFontSize} position={[0, 0, -1]} font="fonts\BrokenScript.otf">
+    SOHIER
+  </Text>
         
         <group ref={groupRef}  position={[position.x, position.y, position.z]} rotation={[rotation.x, rotation.y, rotation.z]}>
-            <mesh {...nodes.Object_6} >
+            <mesh {...nodesRose.Object_6} >
                 <MeshTransmissionMaterial {...materialProps} />
             </mesh>
-            <mesh ref={mesh1} {...nodes.Object_8} position={[position1.x, position1.y, position1.z]} rotation={[rotation1.x, rotation1.y, rotation1.z]} >
+            <mesh ref={mesh1} {...nodesRose.Object_8}  >
                 <MeshTransmissionMaterial {...materialProps} />
             </mesh>
+            {/* <mesh ref={barbed} {...nodesBarbed.Rail_by_daftvid__0}   position={[position1.x, position1.y, position1.z]} rotation={[rotation1.x, rotation1.y, rotation1.z]}>
+                <MeshTransmissionMaterial {...materialProps} />
+            </mesh> */}
+            {/* <mesh ref={mesh1} {...nodes.Object_8} position={[position1.x, position1.y, position1.z]} rotation={[rotation1.x, rotation1.y, rotation1.z]} >
+                <MeshTransmissionMaterial {...materialProps} />
+            </mesh> */}
         </group>
      
         </group>
